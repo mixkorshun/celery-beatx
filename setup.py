5cfe8230a6d72b7a7ab80cdbf1c5d8d53dc1e2b8
mixkorshun <mixkorshun@gmail.com>
2017-08-16 02:40:32 +0300

    upgrade version 0.2.1 -> 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='celery-beatx',
-    version='0.2.1',
+    version='0.3.0',
     url='https://github.com/mixkorshun/celery-beatx',
     description='Adds scheduler and cluster-scheduler for celery',
     keywords=['celery', 'celerybeat', 'scheduler'],
