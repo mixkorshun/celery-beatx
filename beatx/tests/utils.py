e986aab17b4ff37b48f706458cd7ee835e8f4e40
Vladislav Bakin <mixkorshun@gmail.com>
2018-01-26 02:31:16 +0300

    use celery test app creation instead mocking it

--- beatx/tests/utils.py
+++ beatx/tests/utils.py
@@ -1,28 +1,32 @@
 from unittest.mock import Mock
 
+from celery import Celery
+
 
 def get_mock_app(store_url='mock://'):
-    return Mock(
-        conf=Mock(
-            beat_schedule={},
-            beat_store=store_url,
-            beat_store_lock_ttl=60,
+    app = Celery('test_app')
 
-            beat_store_classes={
-                'dummy': 'beatx.store.dummy.Store',
-                'redis': 'beatx.store.redis.Store',
-                'mock': Mock(
-                    spec=type, return_value=Mock(
-                        acquire_lock=Mock(return_value=True),
-                        has_locked=Mock(return_value=True),
-                        load_entries=Mock(return_value={}),
-                    )),
-                'mock+inactive': Mock(
-                    spec=type, return_value=Mock(
-                        acquire_lock=Mock(return_value=False),
-                        has_locked=Mock(return_value=False),
-                        load_entries=Mock(return_value={}),
-                    ))
-            }
-        )
-    )
+    app.config_from_object({
+        'beat_schedule': {},
+        'beat_store': store_url,
+        'beat_store_lock_ttl': 60,
+
+        'beat_store_classes': {
+            'dummy': 'beatx.store.dummy.Store',
+            'redis': 'beatx.store.redis.Store',
+            'mock': Mock(
+                spec=type, return_value=Mock(
+                    acquire_lock=Mock(return_value=True),
+                    has_locked=Mock(return_value=True),
+                    load_entries=Mock(return_value={}),
+                )),
+            'mock+inactive': Mock(
+                spec=type, return_value=Mock(
+                    acquire_lock=Mock(return_value=False),
+                    has_locked=Mock(return_value=False),
+                    load_entries=Mock(return_value={}),
+                ))
+        }
+    })
+
+    return app
